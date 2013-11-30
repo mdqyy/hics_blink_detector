093f623c26dc0f7c56ab9adfffedbb748c6bb793
Nick Sorros <nsorros@gmail.com>
2013-11-30 19:16:44 +0000

    deletion

--- extract_video_number.m
+++
@@ -1,7 +0,0 @@
-function [video_number] = extract_video_number(video_name)
-	
-	% regular expression that extracts the id as it is in corrupted videos
-	% this is the functions that you should change for different video datasets
-	video_number = str2num(regexp(video_name,'\d*','match','once'));
-
-end
